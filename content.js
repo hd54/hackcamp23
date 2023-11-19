743e22b498c150ea77c3a04126f4fcea7ab1671f
Yu Guo <yuguo@dhcp-206-87-222-72.ubcsecure.wireless.ubc.ca>
2023-11-19 10:12:41 -0800

    change selecttext

--- content.js
+++ content.js
@@ -1,6 +1,11 @@
 function getSelectedText() {
-    // Get the selected text in the current window
-    return window.getSelection().toString();
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
 }
   
 function sendTextForExplanation(text) {
