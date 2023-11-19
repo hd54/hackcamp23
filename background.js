997bbb6621ebf899c4e7a47d784a948aaede8686
Yu Guo <yuguo@dhcp-206-87-222-72.ubcsecure.wireless.ubc.ca>
2023-11-19 08:43:38 -0800

    Initial

---
+++ background.js
@@ -0,0 +1,40 @@
+// Replace with your actual OpenAI API key
+const OPENAI_API_KEY = 'YOUR_OPENAI_API_KEY';
+
+// Function to call the OpenAI API
+function callOpenAI(text) {
+  return fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${OPENAI_API_KEY}`
+    },
+    body: JSON.stringify({
+      prompt: text,
+      max_tokens: 150
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.choices && data.choices.length > 0) {
+      return data.choices[0].text.trim();
+    } else {
+      throw new Error('Invalid response from OpenAI API');
+    }
+  })
+  .catch(error => {
+    console.error('Error calling OpenAI API:', error);
+  });
+}
+
+// Listen for messages from the content script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'explainText' && request.text) {
+    callOpenAI(request.text)
+      .then(explanation => sendResponse({ explanation }))
+      .catch(error => sendResponse({ error: error.message }));
+
+    // Return true to indicate that you wish to send a response asynchronously
+    return true;
+  }
+});
---
+++ content.js
@@ -0,0 +1,27 @@
+function getSelectedText() {
+    let text = '';
+    if (window.getSelection) {
+      text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type !== "Control") {
+      text = document.selection.createRange().text;
+    }
+    return text;
+  }
+  
+function sendTextForExplanation(text) {
+    chrome.runtime.sendMessage({ action: 'explainText', text: text }, function(response) {
+      if (response.explanation) {
+        // Display the explanation
+        alert(response.explanation); // You can replace this with a more sophisticated display logic
+      } else if (response.error) {
+        console.error('Error:', response.error);
+      }
+    });
+}
+
+document.addEventListener('mouseup', function() {
+    const selectedText = getSelectedText();
+    if (selectedText.length > 0) {
+      sendTextForExplanation(selectedText);
+    }
+});
