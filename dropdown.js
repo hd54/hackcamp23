5797ae6b112c6b17ca861183f479b314c6466014
MrAnderson1971 <bchu01@student.ubc.ca>
2023-11-19 17:11:14 -0800

    form disappears on successful api response

--- dropdown.js
+++ dropdown.js
@@ -4,6 +4,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         document.getElementById('apiResponse').classList.add('hidden');
     } else if (request.action === "showResponse") {
         document.getElementById('loader').classList.add('hidden');
+        document.getElementById("apiKeyForm").classList.add("hidden");
         document.getElementById('apiResponse').classList.remove('hidden');
         document.getElementById('apiResponse').textContent = request.response;
     }
